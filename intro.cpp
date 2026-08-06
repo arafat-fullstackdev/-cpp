a5c79ca9929d673dc2fa3c79b3387cd54ca1c8fe
Arafat Patwary <farabipsk@gmail.com>
2026-08-06 13:19:54 +0600

    Code string

--- intro.cpp
+++ intro.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 int main(){
     string stingC= "Hello CPP";
-    cout<<stingC<<endl;
+    // cout<<stingC<<endl;
+    string text = "A scuola e treto";
+    // cout<<text<<" "<<text.length()<<endl;
+    // cout<<text<<" "<<text.size()<<endl;
+    cout<<text.at(text.length() -1)<<endl;
+    cout<<text.at(3)<<endl;
+
+    text.at(0)='D';
+    cout<<text<<endl;
 
 return 0;
 
