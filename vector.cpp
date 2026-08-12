85f479c6f098ecd303659eccc70c552adc668f67
Arafat Patwary <farabipsk@gmail.com>
2026-08-12 13:54:56 +0600

    vector code here

---
+++ vector.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>;
+using namespace std;
+
+int main(){
+    vector<string> cars = {"Toyota","Dunlop","Mezde","Tata"};
+
+    // for(string car: cars){
+    //     cout<<car<<"\n"<<endl;
+    // }
+
+    
+    //1. Vector Access
+
+    // cout<<cars[0]<<endl;
+    // cout<<cars[1]<<endl;
+    // cout<<cars[2]<<endl;
+    // cout<<cars[3]<<endl;
+
+//Vector function
+//front()
+cout<<cars.front()<<endl;
+cout<<cars.back()<<endl;
+
+//at()
+return 0;
+
+};
